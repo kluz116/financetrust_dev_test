7636873e14c7295605e652df5a4cef4282297ab1
kidepo <kiyingidenispaul@gmail.com>
2024-06-27 21:48:34 +0300

    Added capabiltiy of using custom .json file fixtures

--- api-assignment/app/Http/Controllers/RecipeController.php
+++ api-assignment/app/Http/Controllers/RecipeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 //@kidepo
 //php artisan l5-swagger:generate
 
@@ -23,8 +24,34 @@ class RecipeController extends Controller
 
     public function __construct()
     {
+        // Default to using a common fixtures file, can be overridden in methods belw
         $json = file_get_contents(storage_path('data.json'));
         $this->data = json_decode($json, true);
+    }
+
+    private function loadData($filePath)
+    {
+        $this->data = json_decode(File::get($filePath), true);
+    }
+
+    public function useCustomFixtureFile(Request $request)
+    {   try {
+            if ($request->has('fixtures_file') && isset($request->fixtures_file)) {
+                $filePath = $request->input('fixtures_file');
+                
+                // Check if the file exists and is a .json file
+                if (File::exists($filePath) && File::extension($filePath) === 'json') {
+                    $this->loadData($filePath);
+                    return true;
+                } else {
+                    return false;
+                }
+            }else{
+                return true;//use default if fixtures_file not attached
+            }
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 
      /**
@@ -34,13 +61,25 @@ class RecipeController extends Controller
      *     tags={"Recipes Statistics API"},
      *     summary="Get the count of unique recipes",
      *     description="Returns the Count of unique recipe names.",
+     *     @OA\Parameter(
+     *         name="fixtures_file",
+     *         in="query",
+     *         required=false,
+     *         @OA\Schema(type="string"),
+     *         description="Path to custom .json fixtures file (optional)"
+     *     ),
      *     @OA\Response(response=200, description="Successful operation"),
      *     @OA\Response(response=400, description="Bad request"),
      *     @OA\Response(response=404, description="Resource not found"),
      * )
      */
-    public function uniqueRecipeCount()
+    public function uniqueRecipeCount(Request $request)
     {
+       
+        if(!$this->useCustomFixtureFile($request)){
+           return response()->json(['error' => 'Invalid file ['.$request->fixtures_file.']. Please provide a valid .json fixtures file else dont pass fixtures_file to use default.'], 400);
+        }
+
         $recipes = array_column($this->data, 'recipe');
         $uniqueRecipes = array_unique($recipes);
         return response()->json(['unique_recipes_count' => count($uniqueRecipes)]);
@@ -53,13 +92,25 @@ class RecipeController extends Controller
      *     tags={"Recipes Statistics API"},
      *     summary="Get the count per recipe",
      *     description="Returns Count number of occurences for each unique recipe name (alphabetically ordered by recipe name).",
+     *     @OA\Parameter(
+     *         name="fixtures_file",
+     *         in="query",
+     *         required=false,
+     *         @OA\Schema(type="string"),
+     *         description="Path to custom .json fixtures file (optional)"
+     *     ),
      *     @OA\Response(response=200, description="Successful operation"),
      *     @OA\Response(response=400, description="Bad request"),
      *     @OA\Response(response=404, description="Resource not found"),
      * )
      */
-    public function countPerRecipe()
+    public function countPerRecipe(Request $request)
     {
+        if(!$this->useCustomFixtureFile($request)){
+            return response()->json(['error' => 'Invalid file ['.$request->fixtures_file.']. Please provide a valid .json fixtures file else dont pass fixtures_file to use default.'], 400);
+        }
+
+         
         $recipes = array_column($this->data, 'recipe');
         $recipeCounts = array_count_values($recipes);
         ksort($recipeCounts);
@@ -73,13 +124,24 @@ class RecipeController extends Controller
      *     tags={"Recipes Statistics API"},
      *     summary="Get the postcode with most delivered recipes",
      *     description="Find the postcode with most delivered recipes.",
+     *     @OA\Parameter(
+     *         name="fixtures_file",
+     *         in="query",
+     *         required=false,
+     *         @OA\Schema(type="string"),
+     *         description="Path to custom .json fixtures file (optional)"
+     *     ),
      *     @OA\Response(response=200, description="Successful operation"),
      *     @OA\Response(response=400, description="Bad request"),
      *     @OA\Response(response=404, description="Resource not found"),
      * )
      */
-    public function busiestPostcode()
+    public function busiestPostcode(Request $request)
     {
+        if(!$this->useCustomFixtureFile($request)){
+            return response()->json(['error' => 'Invalid file ['.$request->fixtures_file.']. Please provide a valid .json fixtures file else dont pass fixtures_file to use default.'], 400);
+        } 
+
         $postcodes = array_column($this->data, 'postcode');
         $postcodeCounts = array_count_values($postcodes);
         arsort($postcodeCounts);
@@ -87,28 +149,42 @@ class RecipeController extends Controller
         return response()->json(['busiest_postcode' => $busiestPostcode]);
     }
 
-     /**
-     * @OA\Get(
-     *     path="/api/match-by-name",
-     *     operationId="matchByName",
-     *     tags={"Recipes Statistics API"},
-     *     summary="Get recipes matching specific keywords",
-     *     @OA\Parameter(
-     *         name="keywords",
-     *         in="query",
-     *         required=false,
-     *         @OA\Schema(type="array", @OA\Items(type="string")),
-     *         description="List of keywords to match"
-     *     ),
-     *     description="List the recipe names (alphabetically ordered) that contain in their name one of the following words: [Potato, Veggie, Mushroom]",
-     *     @OA\Response(response=200, description="Successful operation"),
-     *     @OA\Response(response=400, description="Bad request"),
-     *     @OA\Response(response=404, description="Resource not found"),
-     * )
-     */
-    
+/**
+ * @OA\Get(
+ *     path="/api/match-by-name",
+ *     operationId="matchByName",
+ *     tags={"Recipes Statistics API"},
+ *     summary="Get recipes matching specific keywords",
+ *     description="List the recipe names (alphabetically ordered) that contain in their name one of the following words: [Potato, Veggie, Mushroom]",
+ *     @OA\Parameter(
+ *         name="keywords",
+ *         in="query",
+ *         required=false,
+ *         @OA\Schema(
+ *             type="array",
+ *             @OA\Items(type="string")
+ *         ),
+ *         description="List of keywords to match"
+ *     ),
+ *     @OA\Parameter(
+ *         name="fixtures_file",
+ *         in="query",
+ *         required=false,
+ *         @OA\Schema(type="string"),
+ *         description="Path to custom .json fixtures file (optional)"
+ *     ),
+ *     @OA\Response(response=200, description="Successful operation"),
+ *     @OA\Response(response=400, description="Bad request"),
+ *     @OA\Response(response=404, description="Resource not found")
+ * )
+ */
     public function matchByName(Request $request)
     {
+        if(!$this->useCustomFixtureFile($request)){
+            return response()->json(['error' => 'Invalid file ['.$request->fixtures_file.']. Please provide a valid .json fixtures file else dont pass fixtures_file to use default.'], 400);
+        }
+ 
+
         $keywords = $request->input('keywords', ['Potato', 'Veggie', 'Mushroom']);
     
         // Ensure $keywords is an array
